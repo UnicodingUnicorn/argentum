7eb42b0b6dc5ec43943c6b894e6100fcd848b860
orcas <orcas@makerforce.io>
2017-02-18 19:20:12 +0000

    Fixing things

--- frontend/js/views/signup.js
+++ frontend/js/views/signup.js
@@ -1,17 +1,16 @@
-window.onload = function(){
-	var vm = new Vue({
-		el : "#app",
-		data : {
-			message : ""
-		},
-		methods : {
-			signup : function(){
-				var formData = {
-					username : $("#username").val(),
-					password : $("#password").val(),
-					unit : $("#unit").val()
-				};
-				message = "stuff";
+var vm = new Vue({
+	el : "#app",
+	data : {
+		message : "test"
+	},
+	methods : {
+		signup : function(event){
+			var formData = {
+				username : $("#username").val(),
+				password : $("#password").val(),
+				unit : $("#unit").val()
+			};
+			message = "stuff";
 				/*Vue.http.post("/api/createuser", formData, {emulateJSON : true}).then(
 					function(res){
 						return res.json();
@@ -26,7 +25,7 @@ window.onload = function(){
 		}
 	});
 	
-};
+
 /*$(document).ready(function() { 
 	$("#signup").ajaxForm({
 		url : "/api/createuser",

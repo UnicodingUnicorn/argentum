d3da8a0a3475bd9f7ddc85e52f418c10875a5766
orcas <7555ic@gmail.com>
2017-02-27 01:02:19 +0800

    Gah typo

--- frontend/js/views/signup.js
+++ frontend/js/views/signup.js
@@ -16,8 +16,8 @@ var vm = new Vue({
 					this.message = res.body;
 					this.success = res.body.success;
 					if(res.body.success){
-						//Cookies.set("token", req.body.token.token, {expires : req.body.token.expires});
-						document.cookie = "token=" + req.body.token.token + ";max-age=" + req.body.token.expires;
+						Cookies.set("token", res.body.token.token, {expires : res.body.token.expires});
+						//document.cookie = "token=" + res.body.token.token + ";max-age=" + res.body.token.expires;
 						window.location.href = "index.html";
 						//this.message = Cookies.get("token");
 					}else{
